b7714dd03bc8c46d8ca2c77e32276a6b49514f59
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-14 17:52:44 +0900

    Create 3-2.java

---
+++ Java/Practice/3-2.java
@@ -0,0 +1,17 @@
+public class ManTest {
+
+	public static void main(String[] args) {
+		Man man = new Man();
+		man.age = 40;
+		man.name = "James";
+		man.isMarried = true;
+		man.num = 3;
+		
+		System.out.println(man.age);
+		System.out.println(man.name);
+		System.out.println(man.isMarried);
+		System.out.println(man.num);
+		
+	}
+
+}
