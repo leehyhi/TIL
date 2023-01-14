46f371848787d68f040d73904cb2aa22b65e0cc2
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-14 17:52:23 +0900

    Create 3-1.java

---
+++ Java/Practice/3-1.java
@@ -0,0 +1,22 @@
+public class Man {
+	int age;
+	String name;
+	boolean isMarried;
+	int num; //자식의 수 
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean getIsMarried() {
+		return isMarried;
+	}
+	
+	public int getNum() {
+		return num;
+	}
+}
