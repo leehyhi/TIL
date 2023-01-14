99c4a9578e0ed5839ccaf3e881035c076f303f1a
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-14 17:53:01 +0900

    Create 3-3.java

---
+++ Java/Practice/3-3.java
@@ -0,0 +1,34 @@
+package First;
+
+public class Shop {
+	long OrderNum;
+	String OrderID;
+	String OrderDate;
+	String OrderName;
+	String OrderPN; //ProductNum = PN
+	String OrderAddr;
+	
+	public long getOrderNum() {
+		return OrderNum;
+	}
+	
+	public String getOrderID() {
+		return OrderID;
+	}
+
+	public String getOrderDate() {
+		return OrderDate;
+	}
+	
+	public String getOrderName() {
+		return OrderName;
+	}
+	
+	public String getOrderPN() {
+		return OrderPN;
+	}
+	
+	public String getOrderAddr() {
+		return OrderAddr;
+	}
+}
