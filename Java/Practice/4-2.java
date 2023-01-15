b4b730ec29d824d25d0269ede23e2b7fa64ea819
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-15 14:49:52 +0900

    Create 4-2.java

---
+++ Java/Practice/4-2.java
@@ -0,0 +1,14 @@
+//package One;
+//
+//public class PerosonTest {
+//
+//	public static void main(String[] args) {
+//		Person personKim = new Person();
+//		personKim.name = "김";
+//		personKim.weight = 85.5F;
+//		personKim.height = 180.0F; //디폴드 생성자로  클래스를 생성한 후 인스턴스 변수 값을 따로 초기화 한 경우.
+//		
+//		Person personLee = new Person("이", 175, 75); //인스턴스 변수 초기화와 동시에 클래스 생성, 코드가 간결함.
+//	}
+//	
+//}
