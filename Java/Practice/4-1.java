95237f5334a4cf837b405e190f377d05a6f0c595
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-15 14:48:59 +0900

    //디폴트 생성자, 매개변수로 입력받는 생성자, 생성자 오버로드

---
+++ Java/Practice/4-1.java
@@ -0,0 +1,30 @@
+//package One;
+//
+//public class Person {
+//	String name;
+//	float height;
+//	float weight;
+//	
+//	public Person() {
+//		
+//	}
+//	
+//	public Person(String pname) {
+//		name = pname;
+//	}
+//	
+//	public Person(String pname, float pheight, float pweight) {
+//		name = pname;
+//		height = pheight;
+//		weight = pweight;
+//	}
+//}
+
+package One;
+
+public class Person {
+	int PersonID;
+	String PersonName;
+	Subject korean;
+	Subject math;
+} //참조 자료형, 클래스를 분리하면 편리하게 사용할 수 있음. 
