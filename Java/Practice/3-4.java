0ad66c37e4ddd0aba1b29066bdc44b5795337f0b
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-15 14:11:55 +0900

    Update 3-4.java

--- Java/Practice/3-4.java
+++ Java/Practice/3-4.java
@@ -11,12 +11,12 @@ public class ShopTest {
 		shop.OrderPN = "PD0345-12";
 		shop.OrderAddr = "서울시 영등포구 여의도동 20번지";
 		
-		System.out.println(shop.OrderNum);
-		System.out.println(shop.OrderID);
-		System.out.println(shop.OrderDate);
-		System.out.println(shop.OrderName);
-		System.out.println(shop.OrderPN);
-		System.out.println(shop.OrderAddr);
+		System.out.println(shop.getOrderNum());
+		System.out.println(shop.getOrderID());
+		System.out.println(shop.getOrderDate());
+		System.out.println(shop.getOrderName());
+		System.out.println(shop.getOrderPN());
+		System.out.println(shop.getOrderAddr());
 
 	}
 
