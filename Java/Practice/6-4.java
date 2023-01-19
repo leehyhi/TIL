4c755b4b1e6443752b0bd4d372c8ffd6e2686d7a
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-19 21:36:34 +0900

    Update 6-4.java

--- Java/Practice/6-4.java
+++ Java/Practice/6-4.java
@@ -5,6 +5,7 @@ public class TakeTrans {
 	public static void main(String[] args) {
 		Student studentJames = new Student("James", 5000);
 		Student studentTomas = new Student("Tomas", 10000);
+		Student studentEdward = new Student("Edward", 20000);
 		
 		Bus bus100 = new Bus(100);
 		studentJames.takeBus(bus100);
@@ -15,6 +16,11 @@ public class TakeTrans {
 		studentTomas.takeSubway(subwayGreen);
 		studentTomas.showInfo();
 		subwayGreen.showInfo();
+		
+		Texi texi2341 = new Texi(2341);
+		studentEdward.takeTexi(texi2341);
+		studentEdward.showInfo();
+		texi2341.showinfo();
 	}
 
 }
