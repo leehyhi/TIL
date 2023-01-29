45f5a5eb58e9dfe81cdca085b29cca9835081942
leehyhi <121209443+leehyhi@users.noreply.github.com>
2023-01-29 21:31:05 +0900

    Create 3.c

---
+++ C/ProjectC/3.c
@@ -0,0 +1,46 @@
+// #include <stdio.h>
+
+// // 메인 함수 위 쪽에서 함수 정의.
+// void p(int num);
+// int add(int num1, int num2);
+// int sub(int num1, int num2);
+// int mul(int num1, int num2);
+// int div(int num1, int num2);
+
+// int main(void) {
+//     int num = 2;
+//     num = add(num, 3);
+//     p(num);
+
+//     num = sub(num, 1);
+//     p(num);
+
+//     num = mul(num, 3);
+//     p(num);
+
+//     num = div(num, 6);
+//     p(num);
+
+//     return 0;
+// }
+
+// // 메인 함수 아래쪽에서 함수 입력.
+// void p(int num) {
+//     printf("num은 %d입니다.\n", num);
+// }
+
+// int add(int num1, int num2) {
+//     return num1 + num2;
+// }
+
+// int sub(int num1, int num2) {
+//     return num1 - num2;
+// }
+
+// int  mul(int num1, int num2) {
+//     return num1 * num2;
+// }
+
+// int div(int num1, int num2) {
+//     return num1 / num2;
+// }
